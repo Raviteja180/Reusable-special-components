9f83d571df36beebbd5deeb95de99518b49b7b2d
Raviteja180 <tatipamularaviteja525@gmail.com>
2023-01-21 20:14:19 +0530

    added h1 tag

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App(){
        <div><Button>Buy Now!</Button></div>
        <div><Button>See Deal</Button></div>
        <div><Button>Hide Ads</Button></div>
+       <h1>Raviteja is working</h1>
     </div>
 }
 
