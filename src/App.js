a69033d642579598f1b05db7150c7daafec9d172
Raviteja180 <tatipamularaviteja525@gmail.com>
2023-01-23 16:39:52 +0530

    accordian created with opening closing icons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
- import Button from "./Button";
+ import Button from "./components/Button";
+ import { DiAndroid } from "react-icons/di";
+ import { FaAmazonPay,FaAngular } from "react-icons/fa";
+ import  Accordian from './components/Accordion'
 function App(){
-    return <div>
-       <div><Button success >click me</Button></div>
-       <div><Button>Buy Now!</Button></div>
-       <div><Button>See Deal</Button></div>
-       <div><Button>Hide Ads</Button></div>
-       <h1>Raviteja is working</h1>
-       <h2>ratnakar is also working</h2>
-    </div>
+    const items = [
+    {
+        id:1,
+        label : 'can i use React on a  project?',
+        content : 'yes you can use'
+    },
+    {
+        id:2,
+        label : 'can i use Javascript on a  project?',
+        content : 'yes you can use'
+    },
+    {   
+        id:3,
+        label : 'can i use CSS on a  project?',
+        content : 'yes you can use'
+    }]
+   return <Accordian items={items}/>
 }
 
 export default App;
---
+++ src/pages/ButtonPage.js
@@ -0,0 +1,17 @@
+import Button from "../components/Button";
+import { DiAndroid } from "react-icons/di";
+import { FaAmazonPay,FaAngular } from "react-icons/fa";
+function ButtonPage(){
+   const handleClick = ()=>{
+       console.log("handleclick");
+   }
+   return <div>
+      <div><Button success rounded outline className="mb-10" onClick={handleClick}><DiAndroid /> click me</Button></div>
+      <div><Button warning outline ><FaAmazonPay/>Buy Now!</Button></div>
+      <div><Button danger outline ><FaAngular />See Deal</Button></div>
+      <div><Button secondary outline rounded >Hide Ads</Button></div>
+      <div><Button primary rounded outline>something primary</Button></div>
+   </div>
+}
+
+export default ButtonPage;
